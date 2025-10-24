fa1fbdfd3fd17258a7d8f286358fb95a9eb18f5e
Blomblo <63555404+mrblomblo@users.noreply.github.com>
2025-10-24 19:11:40 +0200

    Change app version

--- backend/app/main.py
+++ backend/app/main.py
@@ -8,7 +8,7 @@ from .database import get_db, init_db, init_engine
 from .routes import admin, media, tags, search, sharing
 from datetime import datetime
 
-app = FastAPI(title="Blombooru", version="1.13.3")
+app = FastAPI(title="Blombooru", version="1.16.5")
 
 # Mount static files
 static_path = Path(__file__).parent.parent.parent / "frontend" / "static"
