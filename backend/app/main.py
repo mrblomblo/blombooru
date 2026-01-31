5b8f1fe66479aad63301f4e2ecda467f1f3a6912
Blomblo <63555404+mrblomblo@users.noreply.github.com>
2026-01-31 04:38:49 +0100

    Update language file & Bump version

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ from .auth_middleware import AuthMiddleware
 from .translations import translation_helper, language_registry
 from datetime import datetime
 
-APP_VERSION = "1.27.5"
+APP_VERSION = "1.27.25"
 
 app = FastAPI(title="Blombooru", version=APP_VERSION)
 app.add_middleware(AuthMiddleware)
