5b0c7136ba9017d87802f2c20a45736b08228177
Blomblo <63555404+mrblomblo@users.noreply.github.com>
2026-02-19 19:38:15 +0100

    Move declarative_base import

--- backend/app/shared_tag_models.py
+++ backend/app/shared_tag_models.py
@@ -1,6 +1,5 @@
 from sqlalchemy import Column, Integer, String, DateTime, Enum, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql import func
 import enum
 
